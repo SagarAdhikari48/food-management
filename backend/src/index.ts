7e19fc2aee4fbc85fdc83d56a147dcae5a120a0e
Sagar Adhikari <sagradhkr48@gmail.com>
2025-07-04 21:46:14 +0545

    configured project

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Welcome to the Food Order App API",})
+});
+
+app.listen(7000, () => {
+    console.log("Server is running on port 7000");
+})
